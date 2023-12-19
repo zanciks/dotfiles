17222748510e18842a32f33a3d5854fe151d34d2
Zanciks <Zackarysemancik@gmail.com>
2023-12-18 18:50:59 -0700

    Delete Code - OSS directory

--- Code - OSS/User/History/-47b10e9f/7061.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let test = 2;
-    if test == 4 {
-        println!("Hello, world!");
-    }
-}
--- Code - OSS/User/History/-59fd050e/SkjM.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Terminate any polybar which is running
-killall -q polybar
-
-# Wait for them to close 
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-polybar wpgtk &
